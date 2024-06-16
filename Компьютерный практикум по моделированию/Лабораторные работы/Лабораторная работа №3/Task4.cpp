dcc815e3dc99abfc8636fb61a4f3131fec2c8fc2
Anastasiya Ignatova <147916582+LilNasTyan@users.noreply.github.com>
2024-06-17 00:41:44 +0300

    Add files via upload

---
+++ Компьютерный практикум по моделированию/Лабораторные работы/Лабораторная работа №3/Task4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double x;
+    cout << "Введите значение x: ";
+    cin >> x;
+
+    double sum = 0.0;
+    double eps = 1e-6; // заданная точность
+
+    int k = 1;
+    double term; // Объявляем term здесь
+
+    do {
+        // Вычисляем текущий член ряда: (-1)^k / (sin(kx) + 6 + k^2)
+        term = pow(-1.0, k) / (sin(k * x) + 6 + k * k);
+
+        // Добавляем текущий член ряда к сумме
+        sum += term;
+
+        // Увеличиваем k для следующей итерации
+        k++;
+
+    } while (fabs(term) >= eps); // Продолжаем, пока текущий член ряда больше или равен eps
+
+    // Выводим результат суммы
+    cout << "Сумма ряда с точностью до 10^-6: " << sum << endl;
+
+    return 0;
+}
+
+
